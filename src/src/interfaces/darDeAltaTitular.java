9886361a69f3f167e359e3a91638f146eafe5f58
aleguiter09 <aleguiter9@gmail.com>
2020-10-27 16:59:57 -0300

    Primer cambio historia 9

---
+++ src/src/interfaces/darDeAltaTitular.java
@@ -0,0 +1,29 @@
+package interfaces;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class darDeAltaTitular {
+    private JRadioButton radioButton1;
+    private JPanel panel1;
+    private JButton button1;
+
+
+    public darDeAltaTitular() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null,"HOLA");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new darDeAltaTitular().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
