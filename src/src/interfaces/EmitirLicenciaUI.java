b7f35e1e6f3773fabae7aa6315a10678710547cd
FaustoDavid98 <fdavid@frsf.utn.edu.ar>
2020-10-29 23:19:45 -0300

    Definición de componentes de la UI - Faltan listeners

--- src/src/interfaces/EmitirLicenciaUI.java
+++ src/src/interfaces/EmitirLicenciaUI.java
@@ -1,4 +1,25 @@
 package interfaces;
 
+import javax.swing.*;
+
 public class EmitirLicenciaUI {
+    private JButton botonCancelar;
+    private JButton botonNuevoTitular;
+    private JButton botonConfirmar;
+    private JTextField campoNroLicencia;
+    private JTextField campoTipoDoc;
+    private JTextField campoNroDoc;
+    private JTextField campoNombre;
+    private JTextField campoCUIL;
+    private JComboBox comboClases;
+    private JCheckBox donanteDeOrganosCheckBox;
+    private JTextField campoGrupo;
+    private JTextField campoFechaOtorgamiento;
+    private JTextField campoDireccion;
+    private JTextField campoFechaNacimiento;
+    private JTextArea campoObservaciones;
+    private JLabel lablelTipoClase;
+    private JTextField campoBuscarTitular;
+
+
 }
